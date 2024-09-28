454f75d8c8eddb3d37400a8b67c8d8593686aab0
nikaburduli41 <burdulinika22@gmail.com>
2024-09-28 22:22:30 +0400

    Add files via upload

---
+++ GOA/day 2.py
@@ -0,0 +1,17 @@
+
+FoOOD="shaurma"
+FoOd="salad"
+fOOD="xajapuri"
+FoOD="kvercxi"
+FooD="semwvari kvercxi"
+FOOD="pizza"
+Foood="adjaruli ajapuri"
+food="xhinkal"
+print (food)
+print (Foood)
+print (FOOD)
+print (FooD)
+print (FooD)
+print (FoOD)
+print (fOOD)
+print (FoOD)
