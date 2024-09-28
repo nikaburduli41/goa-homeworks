b97b72d3b17923c0cff7e6a4b6c77097d9c14f7d
nikaburduli41 <burdulinika22@gmail.com>
2024-09-28 21:52:52 +0400

    Add files via upload

---
+++ GOA/day 3.py
@@ -0,0 +1,10 @@
+print (4-4)
+print (3+9)
+print (88-33)
+print (22-2)
+print (2+9)
+#imitom aris rom gvexmareba cvladis saxeli rom vipovot martivad ra gviweria cvladsi
+name="nika"
+surnname="burduli"
+print (name)
+print (surnname)
