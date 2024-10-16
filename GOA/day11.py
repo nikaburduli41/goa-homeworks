a6ca7489781d0e32a3579c2cfb6aa449e1717410
nikaburduli41 <burdulinika22@gmail.com>
2024-10-16 15:10:39 +0400

    Add files via upload

---
+++ GOA/day11.py
@@ -0,0 +1,9 @@
+real_name="nika"
+real_password="NIKANIKA"
+real_email="burdulinika22@gmail.com"
+
+name=input("enter name: ")
+password=input("enter password: ")
+email=input("enter email: ")
+
+print("real_name" == "name" and "real_password" == "password" and "real_email" == "email")
---
+++ GOA/day11part2.py
@@ -0,0 +1,17 @@
+print("true" and "false")
+#იმიტომ აგდებს ანდზე ფალს რადგან რომ შეხვიდე სადმე და არასწორი პაროლი ან სახელი ფალს ამოგიგდებს
+print("true" or "false")
+
+number=10
+num1=200
+num2=211
+num3=333
+num4=555
+num5=111
+
+print("number" < "num1")
+print("num1" < "num2")
+print("num5" > "num3")
+print("num" == "num2")
+print("num4" > "num3")
+
