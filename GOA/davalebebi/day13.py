15c568467ab5be83680cb78223063230bb30e030
nikaburduli41 <burdulinika22@gmail.com>
2024-10-22 16:28:21 +0400

    Add files via upload

---
+++ GOA/davalebebi/day13.py
@@ -0,0 +1,12 @@
+for i in range(0,10):
+    print(i)
+
+for i in range(50,150):
+    print(i)
+
+for i in range(200,500,+2):
+    print(i)
+
+for i in range(0,50,+3):
+    print(i)
+#ლუპს ვიყენებთ რომ ბევრი რიცხვი ან ციფრი რომ დავწეროთ ბევრჯერ
