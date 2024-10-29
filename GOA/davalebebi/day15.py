b14fc503f353196f10a12890338fdc52e79bc2dc
nikaburduli41 <burdulinika22@gmail.com>
2024-10-29 20:45:25 +0400

    Add files via upload

---
+++ GOA/davalebebi/day15.py
@@ -0,0 +1,25 @@
+for i in range (1,10):
+    print (i)
+
+for i in range(50,150):
+    print (i)
+
+for i in range(200,500,+2):
+    print (i)
+
+for i in range(1,50,2):
+    print (i)
+
+#for loops ვიყენებთ მაშინ როცა რაღაცის დაწერა გვინდა ბევრჯერ
+
+for i in range(10,15):
+    print (i)
+
+for i in range(50,0,-1):
+    print (i)
+
+for i in range(10,70,2):
+    print (i)
+
+for i in range(25,55,2):
+    print (i)
