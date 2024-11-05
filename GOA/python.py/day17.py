e4ae2ee459aa0bc11f274e4034ee789dccd38255
nikaburduli41 <burdulinika22@gmail.com>
2024-11-05 22:18:14 +0400

    Add files via upload

---
+++ GOA/python.py/day17.py
@@ -0,0 +1,22 @@
+
+name=input("enter name: ")
+if name == "nika":
+    print("hello")
+else:
+    print("bye")
+
+num=int(input("enter num: "))
+if num == 7:
+    print("perfect")
+elif num > 7:
+   print("more")
+else:
+   print("less")
+
+name=input("enter name: ")
+is_10 = True
+ 
+if name >= "nika" or is_10:
+    print("twins")
+else:
+    print("not twins")
